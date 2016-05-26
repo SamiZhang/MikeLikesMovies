33d1bfcf4f9e198b977c687e48c767ba9d0c5c6b
Allison Paul & Dexter Moran & Sami Zhang <pair+allisonpaul.dextermoran.samizhang@devbootcamp.com>
2016-05-26 09:34:47 -0500

    add home page feature tests

---
+++ spec/features/groupreviews_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User navigating the site' do
+  scenario 'when user goes to the home page' do
+    visit '/'
+    expect(page).to have_content("Most recent reviews")
+  end
+
+  scenario 'when user goes to the home page' do
+    visit '/'
+    expect(page).to have_link("Login")
+  end
+
+  scenario 'when user goes to the home page' do
+    visit '/'
+    expect(page).to have_css("div.movie-cards", count: 4)
+  end
+end
