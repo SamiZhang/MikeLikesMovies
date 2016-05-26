f78bb0fc8ae1167159a8be05049201f60b9ae8f6
Allison Paul & Dexter Moran & Sami Zhang <pair+allisonpaul.dextermoran.samizhang@devbootcamp.com>
2016-05-26 14:59:00 -0500

    add session specs

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe SessionController, type: :controller do
+  let!(:test_user) { User.create(email: "testemail@test.test", password: "password")}
 
+  describe "post #create" do
+    it "logs a user in and gives them a cookie" do
+      session[:user_id] = nil
+      post :create, {email: "testemail@test.test", password: "password"}
+      expect(controller.session[:user_id]).to eq(User.last.id)
+    end
+
+    it "redirects to the user's page" do
+      post :create, {email: "testemail@test.test", password: "password"}
+      expect(:user).to redirect_to("/users/#{test_user.id}" )
+    end
+  end
+
+  describe "post #destroy" do
+    it "logs a user out and clears the session" do
+      delete :destroy
+      expect(controller.session[:user_id]).to eq(nil)
+    end
+  end
 end
