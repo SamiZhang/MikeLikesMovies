191e9696903e8d1f6ad40a221da660d3e1ea0eed
Allison Paul <allison.paul221@gmail.com>
2016-05-26 11:31:05 -0500

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :groupreviews
   resources :users
   root to: "groupreviews#index"
+
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 end
