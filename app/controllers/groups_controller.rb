b4cbe09be77350f984d3206a50e95872a4c55ea3
Sami Zhang <sami.zhang90@gmail.com>
2016-05-27 15:09:08 +0000

    link to show group done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,12 @@ class GroupsController < ApplicationController
     end
   end
 
+  def show
+    @group = Group.find(params[:id])
+    @due_date = @group.group_reviews[0].due_date
+    @movie = @group.movies[0]
+  end
+
   private
   def user_params
     params.require(:group).permit(:group_name)
